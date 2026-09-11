a82d3c6f9291d814fa14f9733e950e887a5f7deb
Your Name <your@email.com>
2026-09-11 08:08:39 +0530

    fix(seed): add clean process exit after prisma disconnect

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -146,10 +146,11 @@ async function main() {
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
+    process.exit(0);
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
